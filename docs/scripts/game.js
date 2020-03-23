530183acfca595e02f90433913b0a9087f8a7547
Blackwell <61912692+JoBlackwell@users.noreply.github.com>
2020-03-23 09:35:12 -0600

    start scene

--- docs/scripts/game.js
+++ docs/scripts/game.js
@@ -11,4 +11,45 @@
 // DESCRIPTION:		Controls the heart of Word Flight
 // LAST UPDATE:		03/22/2020 - Created main game.js file to work from
 //========================================================================
-// Made a change
+//START SCENE
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.bttn {
+	margin: auto;
+    width: 50%;
+	border: 3px dash black;
+    color: black;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+.play {background-color:#4CAF50;}
+.play: hover {background-color: #46a049;}
+
+.leaderboard {background-color: #ffff00;}
+.leaderboard: hover {background: #ffffe0;}
+
+.quit {background-color: #f44336;}
+.quit: over {background: #da190b;}
+
+</style>
+</head>
+
+<body>
+
+<h1>Word Flight</h1>
+<h2>Sponsor Logo Here</h2>
+
+
+<button class="play bttn">PLAY</button>
+<button class="leaderbrd bttn">LeaderBoard</button>
+<button class="quit bttn">QUIT</button>
+
+
+</body>
+</html>
+
