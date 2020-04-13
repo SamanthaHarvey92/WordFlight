b8843b5196e183d4104865950a96cd10f0184790
SamanthaHarvey92 <61912567+SamanthaHarvey92@users.noreply.github.com>
2020-04-13 08:38:13 -0600

    Update insert_score.php

--- docs/scripts/insert_score.php
+++ docs/scripts/insert_score.php
@@ -1,16 +1,14 @@
 <?php
 include 'db_connection.php';
 
-$u = intval($_GET['u'])
-
 $conn= $pdo;
 
 $sql = "INSERT INTO leaderboard (user, score) VALUES (:user,:score)";
 
 $stmt= $conn->prepare($sql);
 
-$stmt->bindParam(' :user', $_POST['user'], PD::PARAM_STR);
-$stmt->bindParam(' :score', $_POST['score'], PDO::PARAM_STR)
+$stmt->bindParam(' :user', $_GET['u'], PD::PARAM_STR);
+$stmt->bindParam(' :score', $_GET['s'], PDO::PARAM_STR)
 
 $stmt->exectue();
 
