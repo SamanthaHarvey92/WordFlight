744f2de86880a50244f2b32c057ed93ac5e805e2
BraXVIusPrime <52759442+BraXVIusPrime@users.noreply.github.com>
2020-04-18 00:17:12 +0400

    Code Comments
    Added comments to every file, providing explanations of all necessary sections of code.
    
    Adjusted some formating.
    
    Co-Authored-By: chrismedeiros42 <chrismedeiros42@users.noreply.github.com>

--- docs/scripts/insert_score.php
+++ docs/scripts/insert_score.php
@@ -1,21 +1,29 @@
 <?php
-include 'db_connection.php';
+// Link to the database connection string
+include( "../includes/db_admin.php" );
 
-$conn= $pdo;
-$user = $_GET['u'];
-$score = $_GET['s'];
+// Pull the connection from the database connection string
+$conn = $pdo;
 
+// Get the variables sent from AJAX
+$user = $_GET[ 'u' ];
+$score = $_GET[ 's' ];
 
-$sql = "INSERT INTO leaderboard (user, score) VALUES (:user,:score)";
+// Insert player into the leaderboard
+$sql = "INSERT INTO flywithbutchoharedb_copy.wordflightleaderboard (user, score) VALUES (:user,:score)";
+// Prepare the SQL query statement
+$stmt = $conn->prepare( $sql );
 
-$stmt= $conn->prepare($sql);
+// Bind parameters
+$stmt->bindParam( ':user', $user, PDO::PARAM_STR );
+$stmt->bindParam( ':score', $score, PDO::PARAM_INT );
 
-$stmt->bindParam(':user', $user, PDO::PARAM_STR);
-$stmt->bindParam(':score', $score, PDO::PARAM_INT);
-
+// Perform the SQL query
 $stmt->execute();
 
+// Notify the calling AJAX function of completion
 echo "Input Recieved";
 
+// Clear and close the connection
 $conn = null;
 ?>
